e9096f2f39bed54323eae7eea9530673c81822c3
Michael Paul Coder <mcoder@lucid-code.com>
2024-10-01 14:56:57 +1000

    Update BrainFlow to 5.14

---
+++ BrainFlow/Channels/Restfulness.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace lucidcode.LucidScribe.Plugin.BrainFlow
+{
+    namespace Restfulness
+    {
+        public class PluginHandler : Interface.LucidPluginBase
+        {
+            public override string Name { get { return "Restfulness"; } }
+
+            public override bool Initialize()
+            {
+                try
+                {
+                    return Device.Initialize();
+                }
+                catch (Exception ex)
+                {
+                    throw (new Exception("The '" + Name + "' plugin failed to initialize: " + ex.Message));
+                }
+            }
+
+            public override double Value
+            {
+                get
+                {
+                    return Device.GetMetric(brainflow.BrainFlowMetrics.RESTFULNESS);
+                }
+            }
+
+            public override void Dispose()
+            {
+                Device.Dispose();
+            }
+        }
+    }
+}
